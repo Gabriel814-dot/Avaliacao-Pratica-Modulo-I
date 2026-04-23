8e54135f2357946c8135a1e1491b6b104f439a24
Gabriel Pedro Santos de Lino <lyu3117@gmail.com>
2026-04-23 16:49:33 -0300

    Add files via upload

---
+++ prova_imc.py
@@ -0,0 +1,19 @@
+altura = float(input("digite sua altura"))
+peso = float(input("digite seu peso"))
+imc = peso / altura **2
+
+if imc <= 18.5:
+    print("abaixo do peso")
+elif imc <= 24.9:
+    print("peso normal")
+elif imc <= 29.9:
+    print("sobrepeso")
+elif imc <= 34.9:
+    print("obesidade 1 ")
+elif imc <= 39.9:
+    print("obesidade grau 2")
+elif imc >= 40.0:
+    print("obesidade grau 3")
+else:
+    print("obesidade grau 3")
+
